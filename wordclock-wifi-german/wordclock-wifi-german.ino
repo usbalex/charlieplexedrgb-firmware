8cbc3751d9941e4351c3ccbb6e9369a0131de611
alex <alexanderbleitner@gmail.com>
2020-03-30 22:04:45 +0200

    Fixed summer-/wintertime (summertime was in the wrong direction), readded serial debug output, fixed hour update and made timing all in all more consistent (now _h, _m always point to the current minute and hour independent of idiom, etc.), added bootup screen and indicators during NTP update

--- wordclock-wifi-german/wordclock-wifi-german.ino
+++ wordclock-wifi-german/wordclock-wifi-german.ino
@@ -15,6 +15,28 @@ unsigned int _m = 45; // minute displayed
 unsigned int _h = 11; // hour displayed
 
 /**
+ * In german there are two ways to express the current time.
+ * In the moment you plug in the power, the state of the button controlls which way is used.
+ * 
+ *   (IDIOM == false, btn released) => "viertel eins", "halb eins", "dreiviertel eins", "punkt eins"
+ *   (IDIOM == true, btn pressed) => "viertel nach zwölf", "halb eins", "viertel vor eins", "punkt eins"
+ */
+const boolean IDIOM = !digitalRead(BUTTON);
+
+// Colors in which a LED can light up.
+enum color {
+  BLACK = 0,
+  RED = 1,
+  GREEN = 2,
+  YELLOW = 3,
+  BLUE = 4,
+  PURPLE = 5,
+  AQUA = 6,
+  WHITE = 7
+};
+
+
+/**
  * Definition of LEDs that need be be light up in order to show
  * a specific word on the word clock.
  */
@@ -43,6 +65,13 @@ const unsigned int vier[] = {69, 70, 71, 72, 0};
 const unsigned int acht[] = {73, 74, 75, 76, 0};
 const unsigned int zehn[] = {77, 78, 79, 80, 0};
 
+// Loading (0..4) Right column
+const unsigned int loadingLEDS[] = {80, 64, 48, 32, 16, 0};
+const unsigned int loadingColors[] = {GREEN, GREEN, YELLOW, YELLOW, RED, 0};
+
+// Init ("ZEIT")
+const unsigned int initLEDS[] = {4, 7, 10, 13, 0};
+
 /**
  * Definition of the pins that are connected to an LED.
  * For LED n the pin:
@@ -52,86 +81,86 @@ const unsigned int zehn[] = {77, 78, 79, 80, 0};
  *   (n*4)-1 => blue
  */
 const unsigned int LEDMAP[] = {
-  2, 3, 4, 5,
-  3, 2, 4, 5,
-  4, 2, 3, 5,
-  5, 2, 3, 4,
-  6, 2, 3, 4,
-  7, 2, 3, 4,
-  8, 2, 3, 4,
-  9, 2, 3, 4,
-  10, 2, 3, 4,
-  11, 2, 3, 4,
-  12, 2, 3, 4,
-  14, 2, 3, 4,
-  15, 2, 3, 4,
-  16, 2, 3, 4,
-  17, 2, 3, 4,
-  18, 2, 3, 4,
-  2, 6, 7, 8,
-  3, 6, 7, 8,
-  4, 6, 7, 8,
-  5, 6, 7, 8,
-  6, 5, 7, 8,
-  7, 5, 6, 8,
-  8, 5, 6, 7,
-  9, 5, 6, 7,
-  10, 5, 6, 7,
-  11, 5, 6, 7,
-  12, 5, 6, 7,
-  14, 5, 6, 7,
-  15, 5, 6, 7,
-  16, 5, 6, 7,
-  17, 5, 6, 7,
-  18, 5, 6, 7,
-  2, 9, 10, 11,
-  3, 9, 10, 11,
-  4, 9, 10, 11,
-  5, 9, 10, 11,
-  6, 9, 10, 11,
-  7, 9, 10, 11,
-  8, 9, 10, 11,
-  9, 8, 10, 11,
-  10, 8, 9, 11,
-  11, 8, 9, 10,
-  12, 8, 9, 10,
-  14, 8, 9, 10,
-  15, 8, 9, 10,
-  16, 8, 9, 10,
-  17, 8, 9, 10,
-  18, 8, 9, 10,
-  2, 12, 14, 15,
-  3, 12, 14, 15,
-  4, 12, 14, 15,
-  5, 12, 14, 15,
-  6, 12, 14, 15,
-  7, 12, 14, 15,
-  8, 12, 14, 15,
-  9, 12, 14, 15,
-  10, 12, 14, 15,
-  11, 12, 14, 15,
-  12, 11, 14, 15,
-  14, 11, 12, 15,
-  15, 11, 12, 14,
-  16, 11, 12, 14,
-  17, 11, 12, 14,
-  18, 11, 12, 14,
-  2, 16, 17, 18,
-  3, 16, 17, 18,
-  4, 16, 17, 18,
-  5, 16, 17, 18,
-  6, 16, 17, 18,
-  7, 16, 17, 18,
-  8, 16, 17, 18,
-  9, 16, 17, 18,
-  10, 16, 17, 18,
-  11, 16, 17, 18,
-  12, 16, 17, 18,
-  14, 16, 17, 18,
-  15, 16, 17, 18,
-  16, 15, 17, 18,
-  17, 15, 16, 18,
-  18, 15, 16, 17
+  2, 3, 4, 5,     //  "1" == "K"
+  3, 2, 4, 5,     //  "2" == "U"
+  4, 2, 3, 5,     //  "3" == "R"
+  5, 2, 3, 4,     //  "4" == "Z" INIT/ZEIT
+  6, 2, 3, 4,     //  "5" == "D"
+  7, 2, 3, 4,     //  "6" == "R"
+  8, 2, 3, 4,     //  "7" == "E" INIT/ZEIT
+  9, 2, 3, 4,     //  "8" == "I"
+  10, 2, 3, 4,    //  "9" == "V"
+  11, 2, 3, 4,    // "10" == "I" INIT/ZEIT
+  12, 2, 3, 4,    // "11" == "E"
+  14, 2, 3, 4,    // "12" == "R"
+  15, 2, 3, 4,    // "13" == "T" INIT/ZEIT
+  16, 2, 3, 4,    // "14" == "E"
+  17, 2, 3, 4,    // "15" == "L"
+  18, 2, 3, 4,    // "16" == "Ä" LOAD
+  2, 6, 7, 8,     // "17" == "V"
+  3, 6, 7, 8,     // "18" == "O"
+  4, 6, 7, 8,     // "19" == "R"
+  5, 6, 7, 8,     // "20" == "N"
+  6, 5, 7, 8,     // "21" == "A"
+  7, 5, 6, 8,     // "22" == "C"
+  8, 5, 6, 7,     // "23" == "H"
+  9, 5, 6, 7,     // "24" == "P"
+  10, 5, 6, 7,    // "25" == "U"
+  11, 5, 6, 7,    // "26" == "N"
+  12, 5, 6, 7,    // "27" == "K"
+  14, 5, 6, 7,    // "28" == "T"
+  15, 5, 6, 7,    // "29" == "H"
+  16, 5, 6, 7,    // "30" == "A"
+  17, 5, 6, 7,    // "31" == "L"
+  18, 5, 6, 7,    // "32" == "B" LOAD
+  2, 9, 10, 11,   // "33" == "Z"
+  3, 9, 10, 11,   // "34" == "W"
+  4, 9, 10, 11,   // "35" == "Ö"
+  5, 9, 10, 11,   // "36" == "L"
+  6, 9, 10, 11,   // "37" == "F"
+  7, 9, 10, 11,   // "38" == "U"
+  8, 9, 10, 11,   // "39" == "N"
+  9, 8, 10, 11,   // "40" == "F"
+  10, 8, 9, 11,   // "41" == "Z"
+  11, 8, 9, 10,   // "42" == "W"
+  12, 8, 9, 10,   // "43" == "E"
+  14, 8, 9, 10,   // "44" == "I"
+  15, 8, 9, 10,   // "45" == "N"
+  16, 8, 9, 10,   // "46" == "S"
+  17, 8, 9, 10,   // "47" == "A"
+  18, 8, 9, 10,   // "48" == "U" LOAD
+  2, 12, 14, 15,  // "49" == "S"
+  3, 12, 14, 15,  // "50" == "E"
+  4, 12, 14, 15,  // "51" == "C"
+  5, 12, 14, 15,  // "52" == "H"
+  6, 12, 14, 15,  // "53" == "S"
+  7, 12, 14, 15,  // "54" == "I"
+  8, 12, 14, 15,  // "55" == "E"
+  9, 12, 14, 15,  // "56" == "B"
+  10, 12, 14, 15, // "57" == "E"
+  11, 12, 14, 15, // "58" == "N"
+  12, 11, 14, 15, // "59" == "E"
+  14, 11, 12, 15, // "60" == "U"
+  15, 11, 12, 14, // "61" == "N"
+  16, 11, 12, 14, // "62" == "E"
+  17, 11, 12, 14, // "63" == "L"
+  18, 11, 12, 14, // "64" == "F" LOAD
+  2, 16, 17, 18,  // "65" == "D"
+  3, 16, 17, 18,  // "66" == "R"
+  4, 16, 17, 18,  // "67" == "E"
+  5, 16, 17, 18,  // "68" == "I"
+  6, 16, 17, 18,  // "69" == "V"
+  7, 16, 17, 18,  // "70" == "I"
+  8, 16, 17, 18,  // "71" == "E"
+  9, 16, 17, 18,  // "72" == "R"
+  10, 16, 17, 18, // "73" == "A"
+  11, 16, 17, 18, // "74" == "C"
+  12, 16, 17, 18, // "75" == "H"
+  14, 16, 17, 18, // "76" == "T"
+  15, 16, 17, 18, // "77" == "Z"
+  16, 15, 17, 18, // "78" == "E"
+  17, 15, 16, 18, // "79" == "H"
+  18, 15, 16, 17  // "80" == "N" LOAD
 };
 
 /** 
@@ -144,27 +173,6 @@ unsigned int _screen[] = {
   2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
-
-/**
- * In german there are two ways to express the current time.
- * In the moment you plug in the power, the state of the button controlls which way is used.
- * 
- *   (IDIOM == false, btn released) => "viertel eins", "halb eins", "dreiviertel eins", "punkt eins"
- *   (IDIOM == true, btn pressed) => "viertel nach zwölf", "halb eins", "viertel vor eins", "punkt eins"
- */
-const boolean IDIOM = !digitalRead(BUTTON);
-
-// Colors in which a LED can light up.
-enum color {
-  BLACK = 0,
-  RED = 1,
-  GREEN = 2,
-  YELLOW = 3,
-  BLUE = 4,
-  PURPLE = 5,
-  AQUA = 6,
-  WHITE = 7
 };
 
 struct date {
@@ -193,6 +201,10 @@ void adjustSummertime(date &currentDate);
 void setNewTime(uint8_t hour, uint8_t minute);
 void drawTime(void);
 
+void drawInitPermanent(void);
+void drawLoadingPermanent(int level);
+void clearPermanent(void);
+void drawError(void);
 
 
 SoftwareSerial ESPserial(19, 13); // RX, TX
@@ -205,11 +217,7 @@ date currentDate;
  * \brief Initially set all used pins of the Arduino to the desired state
  */
 void setup() {
-  for (int i = 2; i < 19; i++) {
-    if (i != 13) {
-      pinMode(i, INPUT);
-    }
-  }
+  clearPermanent();
   pinMode(BUTTON, INPUT_PULLUP);
 
   // initialize both serial ports:
@@ -217,12 +225,14 @@ void setup() {
 
   ESPserial.begin(9600);
 
+  drawInitPermanent();
+
   serialDelay(4000);
 
-  //Serial.print("DisSLP");
+  Serial.print("DisSLP");
   sendCommand("AT+SLEEP=0");
   
-  //Serial.print("ConfHN");
+  Serial.print("ConfHN");
   sendCommand("AT+CWHOSTNAME=\"Word Clock\"");
 
   char state = 0;
@@ -234,7 +244,7 @@ void setup() {
       Serial.println(state);
     }
 
-    //Serial.print("QrySTAT ... ");
+    Serial.print("QrySTAT ... ");
     if (sendCommand("AT+CIPSTATUS", -100))
     {
       if (ESPserial.find("STATUS:"))
@@ -243,16 +253,16 @@ void setup() {
       }
       else
       {
-        //Serial.println("NoSTAT");
+        Serial.println("NoSTAT");
       }
       // flush
       serialFlush(100);
     }
-    //else
-    //  Serial.println("NoQRY");
+    else
+      Serial.println("NoQRY");
   }
-  //if (state == '2' or state == '3' or state == '4')
-  //  Serial.println("succ");
+  if (state == '2' or state == '3' or state == '4')
+    Serial.println("succ");
 
   setTimezone(timeZone);
 }
@@ -292,6 +302,43 @@ void lightUp(const unsigned int aLed, const enum color aColor) {
   pinMode(lGnd, INPUT);
 }
 
+void lightUpPermanent(const unsigned int aLed, const enum color aColor) {
+  const unsigned int lGnd = LEDMAP[aLed * 4 - 4];
+  const unsigned int lRed = LEDMAP[aLed * 4 - 3];
+  const unsigned int lGreen = LEDMAP[aLed * 4 - 2];
+  const unsigned int lBlue = LEDMAP[aLed * 4 - 1];
+
+  pinMode(lGnd, OUTPUT);
+  digitalWrite(lGnd, 0);
+
+  if (aColor & RED)
+  {
+    pinMode(lRed, OUTPUT);
+    digitalWrite(lRed, 1);
+  }
+
+  if (aColor & GREEN)
+  {
+    pinMode(lGreen, OUTPUT);
+    digitalWrite(lGreen, 1);
+  }
+
+  if (aColor & BLUE)
+  {
+    pinMode(lBlue, OUTPUT);
+    digitalWrite(lBlue, 1);
+  }
+}
+
+void clearPermanent(void)
+{
+  for (int i = 2; i < 19; i++) {
+    if (i != 13) {
+      pinMode(i, INPUT);
+    }
+  }
+}
+
 /**
  * \brief This function modifies the state of the screen and turnd the specified LEDs to the specified color.
  * 
@@ -310,31 +357,29 @@ void draw(const unsigned int aLeds[], const color aColor) {
 /**
  * \brief Switches the clock to the next quarter of the hour
  * 
- *   Moves the clock 15min foreward in time and draws the new time on the screen.
+ *   Moves the clock 1min foreward in time and draws the new time on the screen.
  *   Called either when counter _t is passed or button is pressed for adjustment.
  */
 void nextTime() {
   // Push time forward by 1min
-  _m = (_m + 1) % 60;
-  if (_m == 15 && IDIOM == false)
-    _h = (_h % 12) + 1;
-  if (_m == 30 && IDIOM == true)
-    _h = (_h % 12) + 1;
+  _m = _m + 1;
+
+  while (_m >= 60)
+  {
+    _h = _h + 1;
+    _m = _m - 60;
+  }
+
+  _h = _h % 24;
 
   drawTime();
 }
 
 void setNewTime(uint8_t hour, uint8_t minute) {
-  _m = minute % 60;
+  _m = minute-1; // will be incremented by nextTime()
   _h = hour;
-  if (_m == 15 && IDIOM == false)
-    _h = _h + 1;
-  if (_m == 30 && IDIOM == true)
-    _h = _h + 1;
 
-  _h = _h % 12;
-
-  drawTime();
+  nextTime();
 }
 
 void drawTime(void) {
@@ -343,47 +388,91 @@ void drawTime(void) {
 
   // Randomly choose color of the words
   const unsigned int lC = random(1, 7);
+  uint8_t hour = _h;
 
   // Draw new words
   if ( IDIOM == false ) {
-/*    switch (_m) {
-      case 0: draw(punkt, lC); break;
-      case 15: draw(viertel, lC); break;
-      case 30: draw(halb, lC); break;
-      case 45: draw(dreiviertel, lC); break;      
-    }*/
-    if      (_m < 15) draw(punkt, lC);
-    else if (_m < 30) draw(viertel, lC);
-    else if (_m < 45) draw(halb, lC);
-    else              draw(dreiviertel, lC);
+    if      (_m < 15)  draw(punkt,        lC);
+    else if (_m < 30) {draw(viertel,      lC); hour = _h + 1;}
+    else if (_m < 45) {draw(halb,         lC); hour = _h + 1;}
+    else              {draw(dreiviertel,  lC); hour = _h + 1;}
   } else {
-/*    switch (_m) {
-      case 0: draw(punkt, lC); break;
-      case 15: draw(viertel, lC); draw(nach, lC); break;
-      case 30: draw(halb, lC); break;
-      case 45: draw(viertel, lC); draw(vor, lC); break;
-    }*/
-    if      (_m < 15)  draw(punkt, lC);
-    else if (_m < 30) {draw(viertel, lC); draw(nach, lC);}
-    else if (_m < 45)  draw(halb, lC);
-    else              {draw(viertel, lC); draw(vor, lC);}
+    if      (_m < 15)  draw(punkt,    lC);
+    else if (_m < 30) {draw(viertel,  lC); draw(nach, lC);}
+    else if (_m < 45) {draw(halb,     lC);                hour = _h + 1;}
+    else              {draw(viertel,  lC); draw(vor, lC); hour = _h + 1;}
   }
 
-  switch (_h) {
-    case 1: draw(eins, lC); break;
-    case 2: draw(zwei, lC); break;
-    case 3: draw(drei, lC); break;
-    case 4: draw(vier, lC); break;
-    case 5: draw(funf, lC); break;
-    case 6: draw(sechs, lC); break;
-    case 7: draw(sieben, lC); break;
-    case 8: draw(acht, lC); break;
-    case 9: draw(neun, lC); break;
-    case 10: draw(zehn, lC); break;
-    case 11: draw(elf, lC); break;
-    case 12: draw(zwolf, lC); break;
+  hour = hour % 12;
+
+  switch (hour) {
+    case  1: draw(eins,   lC); break;
+    case  2: draw(zwei,   lC); break;
+    case  3: draw(drei,   lC); break;
+    case  4: draw(vier,   lC); break;
+    case  5: draw(funf,   lC); break;
+    case  6: draw(sechs,  lC); break;
+    case  7: draw(sieben, lC); break;
+    case  8: draw(acht,   lC); break;
+    case  9: draw(neun,   lC); break;
+    case 10: draw(zehn,   lC); break;
+    case 11: draw(elf,    lC); break;
+    case  0: draw(zwolf,  lC); break;
   }
 }
+
+void drawInitPermanent(void)
+{
+  int i = 0;
+  while (initLEDS[i] != 0)
+  {
+    lightUpPermanent(initLEDS[i], AQUA);
+    ++i;
+  }
+}
+
+void drawLoadingPermanent(int level)
+{
+  int i = 0;
+  // clear loading leds
+  while (loadingLEDS[i] != 0)
+  {
+    lightUp(loadingLEDS[i], BLACK);
+    ++i;
+  }
+  // set appropriate ones according to selected level
+  while (level >= 0)
+  {
+    lightUpPermanent(loadingLEDS[level], loadingColors[level]);
+    --level;
+  }
+}
+
+void drawError(void)
+{
+  int i;
+  for (int times = 0; times < 5; ++times)
+  {
+    // clear loading leds
+    i = 0;
+    while (loadingLEDS[i] != 0)
+    {
+      lightUp(loadingLEDS[i], BLACK);
+      ++i;
+    }
+    delay(100);
+    // set them to red
+    i = 0;
+    while (loadingLEDS[i] != 0)
+    {
+      lightUpPermanent(loadingLEDS[i], RED);
+      ++i;
+    }
+    delay(200);
+  }
+  clearPermanent();
+}
+
 
 /**
  * \brief Runloop of the clock
@@ -394,7 +483,7 @@ void drawTime(void) {
 void loop() {
   // Adjust clock time if button is pressed
   if (digitalRead(BUTTON) == false && _delay < millis()) {
-    _m = _m + 14;
+    _m = _m + 4;
     nextTime();
     _t = millis() + 60000;
     _delay = millis() + 500;
@@ -406,10 +495,18 @@ void loop() {
     if (_startup or (_h == 4 and _m < 2))
     {
       _startup = false;
+      clearPermanent();
+      drawLoadingPermanent(0);
       autoTime = getDate(currentDate);
-      for (byte i = 5; i > 0 and currentDate.year == 1970; --i)
+      for (byte i = 4; i > 0 and currentDate.year == 1970; --i)
       {
+        drawLoadingPermanent(5-i);
         autoTime = getDate(currentDate);
+      }
+      clearPermanent();
+      if (currentDate.year == 1970)
+      {
+        drawError();
       }
     }
     if (autoTime)
@@ -421,12 +518,12 @@ void loop() {
       uint8_t minute = currentDate.minute;
       uint8_t second = currentDate.second;
 
-      //Serial.print("Time: ");
-      //Serial.print(hour);
-      //Serial.print(":");
-      //Serial.print(minute);
-      //Serial.print(":");
-      //Serial.println(second);
+      Serial.print("Time: ");
+      Serial.print(hour);
+      Serial.print(":");
+      Serial.print(minute);
+      Serial.print(":");
+      Serial.println(second);
       
       // forward 7 minutes, so displayed time is closer to the actual one
       minute += 7;
@@ -487,8 +584,7 @@ void serialDelay(unsigned long delay_time)
 
 bool getDate(date &dateStruct)
 {
-  //Serial.print("Getting date ... ");
-  //Serial.print("Gd ... ");
+  Serial.print("Gd ... ");
   if (sendCommand("AT+CIPSNTPTIME?", -500))
   {
     delay(900);
@@ -515,41 +611,41 @@ bool getDate(date &dateStruct)
 
       if (dateStruct.year == 1970)
       {
-        //Serial.println("1970, retry");
+        Serial.println("1970, retry");
         return false;
       }
       else
       {
-        //Serial.println("succ");
+        Serial.println("succ");
         return true;
       }
     }
     else
     {
-      //Serial.println("NoANS");
+      Serial.println("NoANS");
       return false;
     }
   }
-  //Serial.println("fail");
+  Serial.println("fail");
   return false;
 }
 
 bool sendCommand(String cmd, int check)
 {
-  //if (check >= 0)
-  //  Serial.print(" ... ");
+  if (check >= 0)
+    Serial.print(" ... ");
 
   ESPserial.println(cmd);
   if (check >= 0) {
     serialWaitAvail(check);
     if (ESPserial.find("OK"))
     {
-      //Serial.println("succ");
+      Serial.println("succ");
       return true;
     }
     else
     {
-      //Serial.println("fail");
+      Serial.println("fail");
       return false;
     }
   }
@@ -567,14 +663,14 @@ bool setTimezone(int timeZone)
   char buf[24];
   sprintf(buf, "AT+CIPSNTPCFG=1,%i", timeZone);
   
-  //Serial.print("SetTZ UTC");
-  //if (timeZone < 0)
-  //  Serial.print(timeZone);
-  //else
-  //{
-  //  Serial.print("+");
-  //  Serial.print(timeZone);
-  //}
+  Serial.print("SetTZ UTC");
+  if (timeZone < 0)
+    Serial.print(timeZone);
+  else
+  {
+    Serial.print("+");
+    Serial.print(timeZone);
+  }
   return sendCommand(buf);
 }
 
@@ -645,14 +741,14 @@ void adjustSummertime(date &currentDate)
   {
     if (summer)
     {
-      //Serial.print("Chng Win -> Sum");
-      --currentDate.hour;
+      Serial.println("Chng Win -> Sum");
+      ++currentDate.hour;
       setTimezone(timeZone+summer);
     }
     else
     {
-      //Serial.print("Chng Sum -> Win");
-      ++currentDate.hour;
+      Serial.println("Chng Sum -> Win");
+      --currentDate.hour;
       setTimezone(timeZone);
     }
     currentDate.summer = summer;
